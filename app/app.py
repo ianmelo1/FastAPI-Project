63b28a637ffd362d00918672110bd589f6e10140
ianmelo1 <ianmelo171@gmail.com>
2026-03-01 16:56:30 -0300

    Images, ImageKit IO, up app file

--- app/app.py
+++ app/app.py
@@ -4,6 +4,11 @@ from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
+from app.images import imagekit
+import tempfile
+import shutil
+import os
+import tempfile
 
 
 @asynccontextmanager
@@ -19,16 +24,39 @@ async def upload_file(
         caption: str = Form(""),
         session: AsyncSession = Depends(get_async_session),
 ):
-    post = Post(
-        caption=caption,
-        url="Dummyurl",
-        file_type="photo",
-        file_name="file"
-    )
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
-    return post
+    temp_file_path = None
+
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)  # ✅ corrigido: copyfileobj em vez de copyfile
+
+        upload_result = imagekit.files.upload(
+            file=open(temp_file_path, "rb"),
+            file_name=file.filename,
+            use_unique_file_name=True,
+            tags=["backend-upload"]
+        )
+
+        if upload_result:
+            post = Post(
+                caption=caption,
+                url=f"{os.getenv('IMAGEKIT_URL')}/{upload_result.file_path}",
+                file_type="video" if file.content_type.startswith("video/") else "image",
+                file_name=upload_result.name
+            )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
+
 
 @app.get("/feed")
 async def get_feed(
@@ -40,14 +68,14 @@ async def get_feed(
     posts_data = []
     for post in posts:
         posts_data.append(
-        {
-            "id": str(post.id),
-            "caption": post.caption,
-            "url": post.url,
-            "file_type": post.file_type,
-            "file_name": post.file_name,
-            "created_at": post.created_at.isoformat(),
-        }
-    )
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat(),
+            }
+        )
 
     return {"posts": posts_data}
