4cc2208bb5bd4c199700743481c41af927d1bdc5
ianmelo1 <ianmelo171@gmail.com>
2026-03-10 13:43:28 -0300

    Inclusão do objeto users

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
+import uuid
+
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
-from app.schemas import PostCreate, PostResponse
-from app.db import Post, create_db_and_tables, get_async_session
+from app.schemas import PostCreate, PostResponse, UserCreate, UserUpdate, UserRead
+from app.db import Post, create_db_and_tables, get_async_session, User
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
@@ -9,6 +11,8 @@ import tempfile
 import shutil
 import os
 import tempfile
+from app.users import auth_backend, current_active_user, fastapi_users
+
 
 
 @asynccontextmanager
@@ -18,10 +22,19 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.include_router(fastapi_users.get_auth_router(auth_backend), prefix='/auth/jwt', tags=["auth"])
+app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), prefix='/auth', tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(), prefix='/auth', tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(UserRead), prefix='/auth', tags=["auth"])
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix='/users', tags=["users"])
+
+
+
 @app.post("/upload")
 async def upload_file(
         file: UploadFile = File(...),
         caption: str = Form(""),
+        user: User = Depends(current_active_user),
         session: AsyncSession = Depends(get_async_session),
 ):
     temp_file_path = None
@@ -60,7 +73,8 @@ async def upload_file(
 
 @app.get("/feed")
 async def get_feed(
-        session: AsyncSession = Depends(get_async_session)
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_active_user)
 ):
     result = await session.execute(select(Post).order_by(Post.created_at.desc()))
     posts = [row[0] for row in result.all()]
@@ -79,3 +93,21 @@ async def get_feed(
         )
 
     return {"posts": posts_data}
+
+@app.delete("/posts/{post_id}")
+async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_active_user)):
+    try:
+        post_uuid = uuid.UUID(post_id)
+
+        result = await session.execute(select(Post).where(Post.id == post_uuid))
+        post = result.scalars().first()
+
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+
+        await session.delete(post)
+        await session.commit()
+
+        return {"success": True, "message": "Post deleted"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
